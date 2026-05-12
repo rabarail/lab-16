00182a6842a0850711f9ab12762b42a8f4a189dd
Rajani <rajani.kami004@gmail.com>
2026-05-11 23:38:31 -0400

    Read fire data from CSV into lists

--- Lab16_rbaraili1-#.py
+++ Lab16_rbaraili1-#.py
@@ -11,8 +11,29 @@ Date5/11/2026 """
 import plotly.express as ex
 import csv 
 
-latitdue = []
-longitude = []
+latitudes = []
+longitudes = []
 brightnesses = []
 dates        = []
+
+with open ("world_fires_1_day.csv") as csvfile:
+    reader = csv.reader(csvfile)
+
+    for index, row in enumerate(reader):
+        if index == 0:
+            continue 
+
+        try:
+            latitude = float(row[0])
+            latitudes.append(latitude) 
+            longitude = float(row[1])
+            longitudes.append(longitude)
+            brightness = float(row[2])
+            brightnesses.append(brightness)
+            date = row[5]
+            dates.append(date)
+        except ValueError:
+            print(f"Warning- Skipping row {index} due to invalid data.")
+        
+
 
