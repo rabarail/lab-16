5d3e8ea62da3bb532d41849136fad17cfcd818db
Rajani <rajani.kami004@gmail.com>
2026-05-11 23:08:39 -0400

    starting lab 16

---
+++ Lab16_rbaraili1-#.py
@@ -0,0 +1,12 @@
+"""The program's name,
+author: Rajani Baraili
+The purpose of the program: Reads the world_fires_1_day.csv file that contains one day of
+                global fire detection data from NASA FIRMS, and plots each
+                fire's latitude and longitude on an interactive world map using
+                plotly express.
+Any info about starter code (If used, where it came from, link, etc.), and the: none
+Date5/11/2026 """
+
+latitdue = []
+longitude = []
+
